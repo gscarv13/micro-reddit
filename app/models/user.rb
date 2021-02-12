93c80ea66c20b4948172a966c88b909c65eba302
aliciapaz <aliciapazrojas@gmail.com>
2021-02-12 18:55:26 -0300

    add uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
 
-  validates :username, presence: true, length: { minimum: 2, maximum: 25 }
+  validates :username, presence: true, uniqueness: true, length: { minimum: 2, maximum: 25 }
 end
