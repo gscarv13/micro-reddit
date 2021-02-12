16178d345901f32e03d425d0ae7c2b9c0843ecd2
aliciapaz <aliciapazrojas@gmail.com>
2021-02-12 18:34:15 -0300

    add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :title, presence: true, length: { minimum: 2, maximum: 60 }
+  validates :body, presence: true, length: { minimum: 10, maximum: 600 }
 end
